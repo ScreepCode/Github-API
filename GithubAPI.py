2d7bd0827b3bcd0a58c24cce1983aab4b3cb69a7
Niklas Buse <nicki_bu@gmx.de>
2021-04-04 22:39:10 +0200

    New: getInfos()

--- GithubAPI.py
+++ GithubAPI.py
@@ -6,6 +6,7 @@ from requests.structures import CaseInsensitiveDict
 
 USERNAME = "ScreepCode"
 TOPIC = "reference"
+KEYS = ["name", "html_url", "description", "language"]
 
 class ReposAPI(object):
     def __init__(self):
@@ -17,7 +18,7 @@ class ReposAPI(object):
     def generateAPI(self):
         allPublicRepos = self.getPublicReposFromGitHub()
         filteredRepos = self.filterReposAfterTopic(allPublicRepos)
-        print(len(filteredRepos))
+        repoInfos = self.getInfos(filteredRepos)
 
     def getPublicReposFromGitHub(self):
         url = "https://api.github.com/users/"+ USERNAME +"/repos"
@@ -35,6 +36,16 @@ class ReposAPI(object):
                 allPublic.remove(repo)
 
         return allPublic
+
+    def getInfos(self, filteredRepos):
+        allInfos = []
+        for repo in filteredRepos:
+            repoInfos = {}
+            for key in KEYS:
+               repoInfos[key] = repo[key]
+            
+            allInfos.append(repoInfos)
+        return json.loads(json.dumps(allInfos))
 
 
 API = ReposAPI()
