fed2d1b7ac3cd68520ac52d9bfa4e648cf7cbb38
Niklas Buse <nicki_bu@gmx.de>
2021-04-21 19:41:34 +0200

    Add. Github Infos as Js Var

---
+++ githubInfosJS.js
@@ -0,0 +1,8 @@
+
+var githubRepos = "[" + 
+        '{"name": "Discord-Bot", "html_url": "https://github.com/ScreepCode/Discord-Bot", "description": "Discord Bot based on the Java Discord API (JDA)", "languages": {"Java": 59343}},' + 
+        '{"name": "Github-API", "html_url": "https://github.com/ScreepCode/Github-API", "description": "Simple Tool for JSON based Output of Public Github Repositories with an set fixed topic", "languages": {"Python": 1797}},' + 
+        '{"name": "JSON-Editoren", "html_url": "https://github.com/ScreepCode/JSON-Editoren", "description": "Graphical implementation of a JSON Editor for Websites", "languages": {"Python": 52883}},' + 
+        '{"name": "Twitch-Bot", "html_url": "https://github.com/ScreepCode/Twitch-Bot", "description": "Twitch Bot based on irc.bot for my Twitch Channel ScreepCode", "languages": {"Python": 7292}},' + 
+        '{"name": "Webseite_Hommel_Bilder", "html_url": "https://github.com/ScreepCode/Webseite_Hommel_Bilder", "description": "Webgallery generated from JSON as Javascript Object", "languages": {"HTML": 20085, "CSS": 8695, "JavaScript": 2720}}' + 
+        "]"
